0e40b5d30b44dbad6204393d49e91af7c3435178
ANA KHASANAH <ana.khasanah@capita.com>
2023-02-13 11:42:57 +0000

    SW4 Array repo

---
+++ src/Example1.java
@@ -0,0 +1,13 @@
+public class Example1 {
+    public static void main(String[] args) {
+        // array = collection of data
+        // dataType [] name = { data1, data2, ....};
+        int [] grades = { 10, 10, 9, 5, 7, 9, 9, 10, 4, 8};
+
+        System.out.println(grades[0]);
+        System.out.println(grades[1]);
+        System.out.println(grades[2]);
+        // print the length of the array using .length
+        System.out.println("The length of the array is " + grades.length);
+    }
+}
